3576e0213534cb2afb1d28f106db22cb6705e63f
George Dialektakis <geo4diale@gmail.com>
2019-11-23 12:12:34 +0200

    Created Autoencoder class for Adversarial Autoencoder

--- autoencoder/adversarial_autoencoder/model.py
+++ autoencoder/adversarial_autoencoder/model.py
@@ -34,3 +34,75 @@ def get_bias_variable(name, shape=None, initializer=tf.constant_initializer(valu
 """
 Adversarial Autoencoder
 """
+
+class Autoencoder(object):
+    def __init__(self,domainString, policyType, variable_scope_name):
+        self.cfg, self.learning_rate_params, self.optim_params = get_configuration(domainString, policyType)
+        cfg = self.cfg
+        self.l2 = float(cfg['l2'])
+        self.layers_shape = cfg['layers_shape']
+        self.n_layers = len(self.layers_shape) - 1
+        self.input_dim = cfg['input_dim']  # self.layers_shape[0]
+        self.encode_dim = self.layers_shape[-1]
+        self.train_model = cfg['train_model']
+        self.domainString = cfg['domainString']
+        self.domains = cfg['domains']
+        self.state_dim = cfg['state_dim']
+        self.isMulti = cfg["multi_domain"]
+        self.batch_size = cfg['batch_size']
+        self.state_buffer = []
+        self.dropout_rate = cfg['dropout_rate']
+        self.epochs = cfg['epochs']
+        self.trainable = cfg['trainable']
+        # variational
+        self.latent_size = cfg["latent_size"]
+
+        try:
+            self.learning_rate_params = self.load_learning_rate(domainString, self.learning_rate_params)
+        except:
+            pass
+
+        setup_logger('msg_logger', cfg['msg_logging_dir'], level=logging.INFO)
+
+        """
+            Find where to close the Session
+        """
+        self.sess = tf.Session()
+        # device = self.get_tensorflow_device()
+        # with tf.device(device):
+        with tf.variable_scope("autoencoder_" + variable_scope_name, reuse=tf.AUTO_REUSE):
+            self.create_variables()
+            self.define_operations()
+
+            if cfg['restore_model']:
+                try:
+                    self.restore_variables()
+                    print("Variables restored for " + domainString + ' AAE ' + str(self.layers_shape))
+                except:
+                    print("Variables restore Failed for " + domainString + ' AAE!')
+                    pass
+
+
+
+    def load_learning_rate(self, dstring, learning_rate_params):
+        with open(dstring + '_aae_learning_rate_logs.csv', 'r+') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            lr_list = [entry for entry in reader]
+            lr = float(lr_list[-1][0])
+            if lr != 0.0:
+                learning_rate_params['learning_rate'] = lr
+
+                # erase old values from learning rate log files
+                # writer = csv.writer(csvfile, delimiter=',')
+                # writer.writerow([0.0])
+
+        return learning_rate_params
+
+    def create_variables(self):
+        pass
+
+    def define_operations(self):
+        pass
+
+    def restore_variables(self):
+        pass
