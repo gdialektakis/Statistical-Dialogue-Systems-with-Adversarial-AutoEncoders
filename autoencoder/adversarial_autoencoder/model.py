8af96660b7bb7f3d8ddfd2cdf9b909d2593b7fc6
jarvis103 <geo4diale@gmail.com>
2019-12-09 17:54:12 +0200

    Deleting useless code

--- autoencoder/adversarial_autoencoder/model.py
+++ autoencoder/adversarial_autoencoder/model.py
@@ -17,33 +17,33 @@ from lib.precision import _FLOATX
 import numpy as np
 import csv
 
-def get_weight_variable(name, shape=None, initializer=tf.contrib.layers.xavier_initializer_conv2d()):
-    if shape is None:
-        return tf.get_variable(name)
-    else:
-        return tf.get_variable(name, shape=shape, dtype=_FLOATX, initializer=initializer)
+# def get_weight_variable(name, shape=None, initializer=tf.contrib.layers.xavier_initializer_conv2d()):
+#     if shape is None:
+#         return tf.get_variable(name)
+#     else:
+#         return tf.get_variable(name, shape=shape, dtype=_FLOATX, initializer=initializer)
+#
+#
+# def get_bias_variable(name, shape=None, initializer=tf.constant_initializer(value=0.0, dtype=_FLOATX)):
+#     if shape is None:
+#         return tf.get_variable(name)
+#     else:
+#         return tf.get_variable(name, shape=shape, dtype=_FLOATX, initializer=initializer)
 
-
-def get_bias_variable(name, shape=None, initializer=tf.constant_initializer(value=0.0, dtype=_FLOATX)):
-    if shape is None:
-        return tf.get_variable(name)
-    else:
-        return tf.get_variable(name, shape=shape, dtype=_FLOATX, initializer=initializer)
 
 def dense(x, n1, n2, name):
-        """
-        Used to create a dense layer.
-        :param x: input tensor to the dense layer
-        :param n1: no. of input neurons
-        :param n2: no. of output neurons
-        :param name: name of the entire dense layer.i.e, variable scope name.
-        :return: tensor with shape [batch_size, n2]
-        """
-        with tf.variable_scope(name, reuse=None):
-            weights = tf.get_variable("weights", shape=[n1, n2],
-                                      initializer=tf.random_normal_initializer(mean=0., stddev=0.01))
-            out = tf.matmul(x, weights)
-            return out
+    """
+    Used to create a dense layer.
+    :param x: input tensor to the dense layer
+    :param n1: no. of input neurons
+    :param n2: no. of output neurons
+    :param name: name of the entire dense layer.i.e, variable scope name.
+    :return: tensor with shape [batch_size, n2]
+    """
+    with tf.variable_scope(name, reuse=None):
+        weights = tf.get_variable("weights", shape=[n1, n2], initializer=tf.random_normal_initializer(mean=0., stddev=0.01))
+        out = tf.matmul(x, weights)
+        return out
 
 
 """
@@ -52,10 +52,10 @@ Adversarial Autoencoder
 
 
 class Autoencoder(object):
-    def __init__(self,domainString, policyType, variable_scope_name):
+    def __init__(self, domainString, policyType, variable_scope_name):
         self.cfg, self.learning_rate_params, self.optim_params = get_configuration(domainString, policyType)
         cfg = self.cfg
-        self.l2 = float(cfg['l2'])
+        #self.l2 = float(cfg['l2'])
         self.layers_shape = cfg['layers_shape']
         self.n_layers = len(self.layers_shape) - 1
         self.input_dim = cfg['input_dim']  # self.layers_shape[0]
@@ -70,7 +70,7 @@ class Autoencoder(object):
         self.dropout_rate = cfg['dropout_rate']
         self.epochs = cfg['epochs']
         self.trainable = cfg['trainable']
-        #discriminator model
+        # discriminator model
         self.discr_layers_shape = cfg['discriminator_layers']
 
         try:
@@ -98,14 +98,12 @@ class Autoencoder(object):
                     print("Variables restore Failed for " + domainString + ' AAE!')
                     pass
 
-
-    def create_variables(self):
-        for domain in self.domains:
-            get_weight_variable('W_' + domain, (self.state_dim[domain], self.layers_shape[1]))
-
-        for i in range(1, self.n_layers):
-            get_weight_variable('W' + str(i), (self.layers_shape[i], self.layers_shape[i + 1]))
-
+    # def create_variables(self):
+    #     for domain in self.domains:
+    #         get_weight_variable('W_' + domain, (self.state_dim[domain], self.layers_shape[1]))
+    #
+    #     for i in range(1, self.n_layers):
+    #         get_weight_variable('W' + str(i), (self.layers_shape[i], self.layers_shape[i + 1]))
 
     def encoder(self, X, reuse=False):
         """
@@ -115,10 +113,6 @@ class Autoencoder(object):
         :return: tensor which is the hidden latent variable of the autoencoder.
         """
         with tf.variable_scope("Encoder", reuse=tf.AUTO_REUSE):
-            # W = get_weight_variable('W_' + self.domainString)
-            # Y = tf.nn.tanh(tf.matmul(X, W))
-            #
-            # Y = tf.nn.dropout(Y, keep_prob=self.prob)
 
             if self.n_layers == 3:
                 input_layer = tf.nn.tanh(dense(X, self.input_dim, self.layers_shape[1], 'e_input_layer'))
@@ -132,7 +126,6 @@ class Autoencoder(object):
 
             return latent_variable
 
-
     def decoder(self, Y, reuse=False):
         """
         Decoder part of the autoencoder.
@@ -140,7 +133,6 @@ class Autoencoder(object):
         :param reuse: True -> Reuse the decoder variables, False -> Create or search of variables before creating
         :return: tensor which should ideally be the input given to the encoder.
         """
-
         with tf.variable_scope("Decoder", reuse=tf.AUTO_REUSE):
             X = Y
             if self.n_layers == 3:
@@ -150,7 +142,6 @@ class Autoencoder(object):
                 output = tf.nn.tanh(dense(d_dense3, self.layers_shape[1], self.input_dim, 'd_output'))
 
             return output
-
 
     def discriminator(self, X, reuse=False):
         """
@@ -229,7 +220,7 @@ class Autoencoder(object):
             d_mean_loss = 0
             g_mean_loss = 0
             for _ in range(self.epochs):
-                z_real_dist = np.random.randn(self.encode_dim) * 5.
+                z_real_dist = np.random.randn(self.encode_dim) * 5.  ## check how to generate the desired prior distribution
                 tmp_a_mean_loss, _ = self.sess.run(self.autoencoder_optimizer, feed_dict={self.X_data_placeholder: input_x})
                 tmp_d_mean_loss, _ = self.sess.run(self.discriminator_optimizer,
                                         feed_dict={self.X_data_placeholder: input_x, self.real_distribution: z_real_dist})
